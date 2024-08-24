8c708dda2a78851a651f6fd16a4520212c3835b5
choguun <29241145+choguun@users.noreply.github.com>
2024-08-24 17:20:05 +0700

    refactor

--- src/GP.sol
+++ src/GP.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract GP {
-    
+import {ERC20} from "solmate/tokens/ERC20.sol";
+
+contract GP is ERC20 {
+    constructor() ERC20("Game Point", "GP", 18) {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
 }
