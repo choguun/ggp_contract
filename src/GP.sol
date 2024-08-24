7859c55a01a4a690d119eba7649f356676ddea5d
choguun <29241145+choguun@users.noreply.github.com>
2024-08-24 22:35:05 +0700

    add GP contract

--- src/GP.sol
+++ src/GP.sol
@@ -1,10 +1,57 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.24;
 
-import {ERC20} from "solmate/tokens/ERC20.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+import {IERC4626} from "./IERC4626.sol";
 
-contract GP is ERC20 {
-    constructor() ERC20("Game Point", "GP", 18) {
-        _mint(msg.sender, 1000000 * 10 ** decimals());
+contract GP is ERC20, IERC4626, Ownable {
+    using SafeERC20 for IERC20;
+
+    IERC20 private _asset;
+
+    constructor(IERC20 asset) Ownable(_msgSender())
+        ERC20("Game Point", "GP")
+    {
+        _asset = asset;
+    }
+
+    function deposit(uint256 assets, address receiver) external override returns (uint256 shares) {
+        uint256 totalAssets = totalAssets();
+        uint256 totalShares = totalSupply();
+        if (totalShares == 0) {
+            shares = assets;
+        } else {
+            shares = (assets * totalShares) / totalAssets;
+        }
+
+        _asset.safeTransferFrom(msg.sender, address(this), assets);
+        _mint(receiver, shares);
+
+        return shares;
+    }
+
+    function withdraw(uint256 assets, address receiver, address owner) external override returns (uint256 shares) {
+        uint256 totalAssets = totalAssets();
+        uint256 totalShares = totalSupply();
+
+        shares = (assets * totalShares) / totalAssets;
+
+        if (msg.sender != owner) {
+            uint256 currentAllowance = allowance(owner, msg.sender);
+            require(currentAllowance >= shares, "ERC20: transfer amount exceeds allowance");
+            _approve(owner, msg.sender, currentAllowance - shares);
+        }
+
+        _burn(owner, shares);
+        _asset.safeTransfer(receiver, assets);
+
+        return shares;
+    }
+
+    function totalAssets() public view override returns (uint256) {
+        return _asset.balanceOf(address(this));
     }
 }
