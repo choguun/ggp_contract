da0230753132f215b7b5a2aa2e584a66a89a28c4
choguun <29241145+choguun@users.noreply.github.com>
2024-08-26 22:27:13 +0700

    update stonevault

--- src/StoneVault.sol
+++ src/StoneVault.sol
@@ -2,17 +2,24 @@
 pragma solidity 0.8.24;
 
 import {StoneMinter} from "./StoneMinter.sol";
+import {ETHDummy} from "./ETHDummy.sol";
 
 contract StoneVault {
     // External contracts
     address public minter;
+    address public eth;
     // External contracts
 
-    constructor(address _minter) {
+    constructor(address _minter, address _eth) {
         minter = _minter;
+        eth = _eth;
     }
     
     function mint(address _to, uint256 _amount) external {
+        require(ETHDummy(eth).balanceOf(msg.sender) >= _amount, "not enough balance");
+        ETHDummy(eth).approve(address(this), _amount);
+        ETHDummy(eth).transferFrom(msg.sender, address(this), _amount);
+
         StoneMinter(minter).mint(_to, _amount);
     }
 
